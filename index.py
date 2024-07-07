3d042cd3731c80c1ede4a3cd6e39ca8ca65b1f87
Igor Rocha <igor_roc@hotmail.com.br>
2024-07-07 18:39:02 -0300

    feat: Comentários e exemplos

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+# Para iniciar a execução do programa, execute o comando abaixo no terminal:
+# mpiexec -n 4 python index.py
 from mpi4py import MPI
 
 from process import Process
@@ -7,13 +9,19 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-# Configurações iniciais
+# * Configurações iniciais
+# ? ------ Exemplo com multicast (3 processos)
+# messages = [
+#     {"source": 0, "time": 0, "receivers": []},
+#     {"source": 1, "time": 2, "receivers": []},
+#     {"source": 2, "time": 4, "receivers": []},
+# ]
+# ? ------ Exemplo sem atraso (3 processos)
 messages = [
-    {"source": 0, "time": 0, "receivers": []},
-    {"source": 1, "time": 2, "receivers": []},
-    {"source": 2, "time": 4, "receivers": []},
+    {"source": 0, "time": 0, "receivers": [{"p": 1, "delay": 1}, {"p": 2, "delay": 4}]},
+    {"source": 1, "time": 2, "receivers": [{"p": 0, "delay": 1}, {"p": 2, "delay": 1}]},
 ]
-# ! Exercício do slide
+# ! Exercício do slide (4 processos) (com atraso)
 # messages = [
 #     {"source": 0, "time": 0, "receivers": [{"p": 1, "delay": 1}, {"p": 3, "delay": 6}]},
 #     {"source": 1, "time": 2, "receivers": [{"p": 2, "delay": 1}, {"p": 3, "delay": 5}]},
@@ -26,7 +34,7 @@ messages = config.fill_messages_with_time(messages)
 comm.Barrier()
 
 # Cria a instância do processo
-process = Process(rank, size)
+process = Process(rank, size, debug=False)
 process.start()
 
 global_time = 0
@@ -39,18 +47,19 @@ while global_time <= len(messages):
             process.increment_clock()  # Incrementa o clock do processo apenas uma vez por evento
             current_time = message["time"]
             if not message["receivers"]:  # Se receivers estiver vazio, enviar multicast
-                process.multicast_message()
+                process.multicast_message(current_time)
             else:
                 for dest in message["receivers"]:
                     delay = dest.get("delay", 0)
-                    process.send_message(dest["p"], delay)
+                    process.send_message(dest["p"], current_time, delay)
+    
     global_time += 1
 
     process.synchronize(global_time)
     process.receive_message(global_time)
 
 comm.Barrier()
-process.process_delayed_messages(global_time)  # Processa todas as mensagens atrasadas antes de finalizar
+process.process_delayed_messages(global_time-1)  # Processa todas as mensagens atrasadas antes de finalizar
 process.finish()
 
 # Finaliza o MPI
--- process.py
+++ process.py
@@ -12,6 +12,7 @@ class Process:
         self.debug = debug
         self.event_counter = rank * 100
         self.message_queue = []
+        self.current_time = 0
 
     def increment_clock(self):
         self.clock[self.rank] += 1
@@ -20,39 +21,43 @@ class Process:
         self.event_counter += 1
         return self.event_counter
 
-    def send_message(self, dest, delay=0):
-        send_time = self.comm.Get_rank() + delay
+    def send_message(self, dest, current_time, delay):
+        self.current_time = current_time
+        send_time = current_time + delay
         self.comm.send((self.clock.copy(), send_time), dest=dest)
-        delay_print = f' com atraso de {delay}' if delay > 0 else ''
-        printer.info(self.incremented_counter(), f'{Fore.GREEN}[SEND]{Style.RESET_ALL} P{self.rank} enviou {self.clock}{delay_print} para P{dest}')
+        delay_print = f' com atraso de {delay}t' if delay > 0 else ''
+        printer.info(self.incremented_counter(), f'{Fore.GREEN}[SEND]{Style.RESET_ALL} (T{current_time}) P{self.rank} enviou {self.clock}{delay_print} para P{dest}')
 
-    def multicast_message(self):
+    def multicast_message(self, current_time):
         if self.debug:
             printer.info(self.incremented_counter(), f'P{self.rank} está realizando multicast...')
-        send_time = self.comm.Get_rank()
+        send_time = current_time
+        self.current_time = current_time
         for dest in range(self.size):
             if dest != self.rank:
                 self.comm.send((self.clock.copy(), send_time), dest=dest)
-                if self.debug:
-                    printer.info(self.incremented_counter(), f'P{self.rank} enviou {self.clock} e o tempo {send_time} para {Fore.GREEN}{dest}{Style.RESET_ALL}')
-        printer.info(self.incremented_counter(), f'{Fore.GREEN}[MULTI]{Style.RESET_ALL} P{self.rank} realizou multicast')
+        printer.info(self.incremented_counter(), f'{Fore.GREEN}[MULTI]{Style.RESET_ALL} (T{current_time}) P{self.rank} realizou multicast')
 
     def receive_message(self, current_time):
         status = MPI.Status()
+        self.current_time = current_time
         while self.comm.Iprobe(source=MPI.ANY_SOURCE, status=status):
             source = status.Get_source()
             received_clock, received_time = self.comm.recv(source=source)
             if current_time >= received_time:
                 if self.check_delay_conditions(source, received_clock):
                     self.clock = np.maximum(self.clock, received_clock)
-                    printer.info(self.incremented_counter(), f'{Fore.MAGENTA}[RECEIVE]{Style.RESET_ALL} P{self.rank} recebeu de P{source} {received_clock} e o tempo {received_time}, relógio atualizado para {self.clock}')
+                    printer.info(self.incremented_counter(), f'{Fore.MAGENTA}[RECEIVE]{Style.RESET_ALL} (T{received_time}) P{self.rank} recebeu de P{source} {received_clock}, relógio atualizado para {self.clock}')
                 else:
-                    self.message_queue.append((source, received_clock, received_time))
-                    printer.warning(self.incremented_counter(), f'{Fore.YELLOW}[DELAY]{Style.RESET_ALL} Mensagem de P{source} em espera, pois seu relógio é {self.clock} e o recebido é {received_clock}')
+                    reason = 'clock_condition'
+                    self.message_queue.append((source, received_clock, received_time, reason))
+                    if self.debug:
+                        printer.info(self.incremented_counter(), f'{Fore.YELLOW}[DELAY]{Style.RESET_ALL} (T{received_time}) Mensagem de P{source} em espera, pois seu relógio é {self.clock} e o recebido é {received_clock}')
             else:
-                self.message_queue.append((source, received_clock, received_time))
+                reason = 'arrival_time'
+                self.message_queue.append((source, received_clock, received_time, reason))
                 if self.debug:
-                    printer.warning(self.incremented_counter(), f'{Fore.YELLOW}[DELAY]{Style.RESET_ALL} Mensagem de {source} em espera pois não chegou no tempo {current_time}')
+                    printer.info(self.incremented_counter(), f'{Fore.YELLOW}[DELAY]{Style.RESET_ALL} (T{current_time}) Mensagem de P{source} adicionada em espera, pois seu tempo de chegada é {received_time}t')
 
     def check_delay_conditions(self, source, received_clock):
         condition_1 = received_clock[source] == self.clock[source] + 1
@@ -60,14 +65,19 @@ class Process:
         return condition_1 and condition_2
 
     def process_delayed_messages(self, current_time):
+        self.current_time = current_time
         for message in self.message_queue[:]:
-            source, received_clock, received_time = message
+            source, received_clock, received_time, reason = message
             if current_time >= received_time and self.check_delay_conditions(source, received_clock):
                 self.clock = np.maximum(self.clock, received_clock)
-                printer.info(self.incremented_counter(), f'{Fore.MAGENTA}[RECEIVE]{Style.RESET_ALL} P{self.rank} processou mensagem atrasada de P{source} - {received_clock}, relógio atualizado para {self.clock}')
+                if reason == 'arrival_time':
+                    printer.info(self.incremented_counter(), f'{Fore.MAGENTA}[RECEIVE]{Style.RESET_ALL} (T{current_time}) P{self.rank} recebeu de P{source} {received_clock}, relógio atualizado para {self.clock}')
+                elif reason == 'clock_condition':
+                    printer.info(self.incremented_counter(), f'{Fore.MAGENTA}[RECEIVE]{Style.RESET_ALL} (T{current_time}) P{self.rank} processou mensagem atrasada de P{source} {received_clock}, relógio atualizado para {self.clock}')
                 self.message_queue.remove(message)
 
     def synchronize(self, current_time):
+        self.current_time = current_time
         self.comm.Barrier()
         self.process_delayed_messages(current_time)
 
@@ -76,9 +86,13 @@ class Process:
 
     def finish(self):
         if self.has_pending_messages():
-            printer.error(self.incremented_counter(), f'Finalizou - VC({self.rank}) {self.clock} com mensagens pendentes')
+            printer.error(self.incremented_counter(), f'T{self.current_time} Finalizou - VC({self.rank}) {self.clock} com {Fore.RED}mensagens pendentes{Style.RESET_ALL}')
+            if self.debug:
+                for message in self.message_queue:
+                    source, received_clock, received_time, reason = message
+                    printer.error(self.incremented_counter(), f'(T{received_time}) P{source} enviou {received_clock} para P{self.rank} - {Fore.RED}"{reason}"{Style.RESET_ALL}')
         else:
-            printer.success(self.incremented_counter(), f'Finalizou - VC({self.rank}) {self.clock}')
+            printer.success(self.incremented_counter(), f'T{self.current_time} Finalizou - VC({self.rank}) {self.clock}')
 
         print()
 
